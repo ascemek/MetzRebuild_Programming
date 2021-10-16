8421fa276124f10d5cf966288c2530e5ced31ee4
RileyWood938 <71392554+RileyWood938@users.noreply.github.com>
2021-10-16 16:01:06 -0400

    commented code
    added comments to my code from earlier today

--- Metz Code/javascript/createProfileDisclaimer.js
+++ Metz Code/javascript/createProfileDisclaimer.js
@@ -1,8 +1,9 @@
 document.getElementById("createProfile").onsubmit= function (){
 	fireEvent(), event.preventDefault();
 };
-
-function fireEvent(){
+//I stole this code from my loginChecker script which may explain the preventDefault()
+//-Riley
+function fireEvent(){ 
 	alert("The back-end of account creation and storage is unfinished. Note that despite inputing this information you will not be able to log into your account.");
 	window.location = "profilePreferences.html";
 }
--- Metz Code/javascript/loginChecker.js
+++ Metz Code/javascript/loginChecker.js
@@ -1,7 +1,12 @@
 document.getElementById("login").onsubmit= function (){
 	fireEvent(), event.preventDefault();
 };
-
+/*using preventDefault() there was a workaround for the window.location code. 
+becasue I was trying to change pages partway through the form submission,
+it submitted the form instead of changing pages. This would obviously break
+the backend becasue it cancels submission, but it is a substitute for actual
+backend (ie: checking if the username/password are in a database) so im not too 
+worried about it. -Riley */
 function fireEvent(){
 	if(confirm("Oops! looks like you don't have a Metz profile. Click 'Ok' to create one.")){
 		window.location="createProfile.html";
