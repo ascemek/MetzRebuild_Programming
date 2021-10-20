a10cacbfb2c15974f12df49607ef5a6bb5684b37
RileyWood938 <71392554+RileyWood938@users.noreply.github.com>
2021-10-20 10:38:54 -0400

    added food profile images
    added the food option images and began working on setting up food sorting choices

---
+++ Metz Code/javascript/profilePreferencesScript.js
@@ -0,0 +1,11 @@
+/*
+var tables = document.getElementsByClassName("profileTable")
+var tableData = tables.children;
+tableData.onclick= function(){
+	tableData.style.boxShadow = "1px 1px 10px"
+	};
+	var milk = document.getElementById("milk");
+milk.onMouseDown = function(){
+	milk.style.boxShodow = "1px 1px 10px"
+};
+*/
